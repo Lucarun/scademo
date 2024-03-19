c82976ca6701fd73c8b28fc2c9a19dfc6fd723b5
Zou Luca <zoublue163@163.com>
2024-03-19 15:28:11 +0800

    first commit

---
+++ sca-test/src/main/java/com/example/scatest/ScaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.scatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaTestApplication.class, args);
+    }
+
+}
